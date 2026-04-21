822d64fa23966c538ef075247708cef82239db1f
Rory Coleman <rory.cosmic@yahoo.com>
2026-04-21 16:25:16 +1000

    first commit

---
+++ SeniorLearnProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SeniorLearnProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
